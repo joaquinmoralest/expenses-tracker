0008d1d4f54025a72749be9bc4d8dd641b5680f1
Joaquin Morales <joaquin.moralestr@gmail.com>
2022-10-30 00:49:45 -0300

    Add resume list of expenses and calculate new remaining

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,49 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
 import ProgressBar from './components/ProgressBar'
 
 function App() {
-  const [remaining, setRemainig] = useState(50)
+  const [remaining, setRemaining] = useState(0)
+  const [percentage, setPercentage] = useState(100)
   const [expenses, setExpenses] = useState([])
   const [income, setIncome] = useState(0)
   const [newIncome, setNewIncome] = useState('')
   const [expenseAmount, setExpenseAmount] = useState('')
   const [expenseConcept, setExpenseConcept] = useState('')
 
+  useEffect(() => calculateRemaining())
+
+  function formatAmount(amount) {
+    return new Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(amount)
+  }
+
+  function calculateRemaining() {
+    let totalExpenses = 0
+
+    expenses.forEach((expense) => {
+      totalExpenses = totalExpenses + expense.amount
+    })
+
+    let newRemaining = income - totalExpenses
+    let newPercentage = (newRemaining / income) * 100
+
+    setRemaining(newRemaining)
+    setPercentage(newPercentage.toFixed(0))
+  }
+
   function addExpense(e) {
     e.preventDefault()
 
     const newExpenseToAdd = {
       id: expenses.length + 1,
-      date: new Date().toISOString(),
-      amount: expenseAmount,
+      date: new Date().toISOString().slice(0, 10),
+      amount: Number(expenseAmount),
       concept: expenseConcept,
     }
 
     setExpenses([...expenses, newExpenseToAdd])
+    calculateRemaining()
     setExpenseAmount('')
     setExpenseConcept('')
   }
@@ -29,6 +52,7 @@ function App() {
     e.preventDefault()
 
     setIncome(newIncome)
+    setRemaining(newIncome)
     setNewIncome('')
   }
 
@@ -43,8 +67,6 @@ function App() {
   function handleIncome(e) {
     setNewIncome(e.target.value)
   }
-
-  console.log(expenses)
 
   return (
     <div className="App">
@@ -79,6 +101,26 @@ function App() {
               </span>
             </button>
           </form>
+
+          <div className='resume-list'>
+            {
+              expenses.map((expense) => {
+                return(
+                  <div className='resume-item' key={expense.id}>
+                    <div className='resume-amount'>
+                      <p>{formatAmount(expense.amount)}</p>
+                    </div>
+                    <div className='resume-concept'>
+                      <p>{expense.concept}</p>
+                    </div>
+                    <div className='resume-date'>
+                      <p>{expense.date}</p>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
         </section>
 
         <section className='budget'>
@@ -101,9 +143,9 @@ function App() {
           <h4 className='txt-center mt-3'>Presupuesto</h4>
           <ProgressBar 
             // bgColor='full'
-            remaining={remaining}
+            remaining={percentage}
           />
-          <h5 className='txt-center'>$5000 de ${income}</h5>
+          <h5 className='txt-center'>{formatAmount(remaining)} de {formatAmount(income)}</h5>
         </section>
       </main>
     </div>
